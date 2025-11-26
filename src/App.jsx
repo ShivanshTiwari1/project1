dea92ec6719f4fe425698246bfb61132f4d0a0f1
Shivansh Tiwari <shivansh454777@gmail.com>
2025-11-27 00:48:47 +0530

    new added

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,36 @@
-export default function Square() {
+import { useState } from "react";
+
+function Square() {
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    setValue("X");
+  }
+
+  return (
+    <button className="square" onClick={handleClick}>
+      {value}
+    </button>
+  );
+}
+
+export default function Board() {
   return (
     <>
-      <div>
-        <button className="square">X</button>
-        <button className="square">X</button>
-        <button className="square">X</button>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
       </div>
-      <div>
-        <button className="square">X</button>
-        <button className="square">X</button>
-        <button className="square">X</button>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
       </div>
-      <div>
-        <button className="square">X</button>
-        <button className="square">X</button>
-        <button className="square">X</button>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   );
